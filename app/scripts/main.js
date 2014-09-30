c5e280e892b835a05abe33e89c832da3943fd2e7
Jay Wilson Jr <info@jaywilsonjr.com>
2014-09-30 13:40:39 -0400

    Added HTML elements to the page using backbone views.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,76 @@
-console.log('The Iron Yard Rocks');
+var titles = [
+  {name: 'EMT-Advanced'}, 
+  {name: 'First Responder'}, 
+  {name: 'EMT-Intermediate'}, 
+  {name: 'EMT-Paramedic'},
+  {name: 'Fire Fighter'},
+  {name: 'Captain'},
+  {name: 'Chief'}
+]
+
+var BaseView = Backbone.View.extend( {
+  className: "wrapper",
+  render: function() {
+    $('body').append(this.el);
+  }
+
+});
+
+var base = new BaseView();
+base.render();
+
+var SearchView = BaseView.extend( {
+  tagName: 'input',
+
+  attributes: {
+    'type': 'search',
+    'placeholder': 'Search titles and ranks...',
+  },
+
+  initialize: function() {
+
+    this.render();
+  },
+
+  render: function() {
+    this.$el.text(this.name);
+    $('.wrapper').append(this.el);
+  },
+
+  filterBy: function(val) {
+    this.filteredList = this.collection.filter(function(item) {
+      return item.match(val);
+    }) 
+    this.render();
+  }
+
+});
+
+var searchView = new SearchView();
+
+var ListView = BaseView.extend( {
+  tagName: 'ul',
+  className: 'list_holder'
+});
+
+var ItemView = BaseView.extend( {
+  tagName: 'li',
+  className: 'single_item',
+
+  initialize: function(options) {
+    this.name = options.name;
+  },
+
+  render: function() {
+    this.$el.text(this.name);
+    $('ul').append(this.el);
+  }
+});
+
+var listView = new ListView();
+listView.render();
+
+_.each(titles, function(rank) {
+  var itemView = new ItemView(rank);
+  itemView.render();
+});
--- test/spec/test.js
+++ test/spec/test.js
@@ -3,11 +3,39 @@
 (function () {
     'use strict';
 
-    describe('Give it some context', function () {
-        describe('maybe a bit more context here', function () {
-            it('should run here few assertions', function () {
+    describe('Backbone Homework', function () {
+        describe('Should have a list', function () {
 
+            it('should be an array', function () {
+              expect(titles).to.be.an('array');
             });
+
+            it('should have a object(s) in the array', function () {
+              expect(titles[0]).to.be.an('object');              
+            });
+        });
+
+        describe('Should have HTML elements render to the page', function () {
+          
+          it('should have a <ul> render on the page', function () {
+            expect($('ul.list_holder').length).to.equal(1);
+          });
+
+          it('should have a <li> render to the page', function() {
+            expect($('li.single_item').length).to.equal(7);
+          });
+
+          it('should have a <input> render to the page', function() {
+            expect($('input[type=search]').length).to.equal(1);
+          })
         });
     });
 })();
+
+
+
+
+
+
+
+
